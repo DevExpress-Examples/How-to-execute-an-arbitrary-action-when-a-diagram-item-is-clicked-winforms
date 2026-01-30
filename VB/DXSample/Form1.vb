ad7d09ee136f5976101bca5a7ebb51c402d5d6b0
DevExpressExampleBot <fakeEmail>
2026-01-30 23:21:22 +0400

    Source auto update [skip ci]

--- VB/DXSample/Form1.vb
+++ VB/DXSample/Form1.vb
@@ -1,30 +1,33 @@
-﻿Imports DevExpress.XtraDiagram
+Imports DevExpress.XtraDiagram
 Imports System
-Imports System.Collections.Generic
 Imports System.ComponentModel
-Imports System.Data
 Imports System.Drawing
-Imports System.Linq
-Imports System.Text
-Imports System.Threading.Tasks
-Imports System.Windows
 Imports System.Windows.Forms
 
 Namespace DXSample
-	Partial Public Class Form1
-		Inherits Form
 
-		Public Sub New()
-			InitializeComponent()
-		End Sub
+    Public Partial Class Form1
+        Inherits Form
 
-		Private Sub diagramControl1_Click(ByVal sender As Object, ByVal e As EventArgs) Handles diagramControl1.Click
-			Dim clickedItem = diagramControl1.CalcHitItem(CType(e, MouseEventArgs).Location)
-			Dim tempVar As Boolean = TypeOf clickedItem Is DiagramItem
-			Dim item As DiagramItem = If(tempVar, CType(clickedItem, DiagramItem), Nothing)
-			If tempVar Then
-				MessageBox.Show(owner:=Me, caption:= "An item was clicked", text:= $"Item: {item.GetType()}{Environment.NewLine}Position: {item.Position}", buttons:= MessageBoxButtons.OK, icon:= MessageBoxIcon.Information)
-			End If
-		End Sub
-	End Class
+        Public Sub New()
+            InitializeComponent()
+        End Sub
+
+        Private Sub diagramControl1_Click(ByVal sender As Object, ByVal e As EventArgs)
+            Dim clickedItem = diagramControl1.CalcHitItem(CType(e, MouseEventArgs).Location)
+            Dim item As DiagramItem = Nothing
+            If CSharpImpl.__Assign(item, TryCast(clickedItem, DiagramItem)) IsNot Nothing Then
+                Call MessageBox.Show(owner:=Me, caption:="An item was clicked", text:=$"Item: {item.GetType()} {Environment.NewLine}Position: {item.Position}", buttons:=MessageBoxButtons.OK, icon:=MessageBoxIcon.Information)
+            End If
+        End Sub
+
+        Private Class CSharpImpl
+
+            <Obsolete("Please refactor calling code to use normal Visual Basic assignment")>
+            Shared Function __Assign(Of T)(ByRef target As T, value As T) As T
+                target = value
+                Return value
+            End Function
+        End Class
+    End Class
 End Namespace
